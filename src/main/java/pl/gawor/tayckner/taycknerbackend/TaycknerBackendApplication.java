c8799f7e8360aee711de7460a07ca68854dabb2b
0x41gawor <41gawor@gmail.com>
2021-11-11 23:33:13 +0100

    chore: create spring-boot project #10

---
+++ src/main/java/pl/gawor/tayckner/taycknerbackend/TaycknerBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.gawor.tayckner.taycknerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaycknerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaycknerBackendApplication.class, args);
+	}
+
+}
