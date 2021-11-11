38e7ccd7271232030b330aad7e264eb2bafa7e28
0x41gawor <41gawor@gmail.com>
2021-11-12 00:36:10 +0100

    feat(repository): create entity class for Category #12

---
+++ src/main/java/pl/gawor/tayckner/taycknerbackend/repository/entity/CategoryEntity.java
@@ -0,0 +1,101 @@
+package pl.gawor.tayckner.taycknerbackend.repository.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * Entity class for `Category`.
+ *
+ * A simple POJO class with Hibernate annotations.
+ */
+@Entity
+@Table(name = "category")
+public class CategoryEntity {
+// -------------------------------------------------------------------------------------- F I E L D S
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "id")
+    private long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "color", nullable = false)
+    private String color;
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH}, fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+// -------------------------------------------------------------------------------------- C O N S T R U C T O R S
+    public CategoryEntity() {
+        // required by Hibernate
+    }
+
+    public CategoryEntity(long id, String name, String description, String color, UserEntity user) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.color = color;
+        this.user = user;
+    }
+
+// -------------------------------------------------------------------------------------- G E T T E R S / S E T T E R S
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+// -------------------------------------------------------------------------------------- T O  S T R I N G
+    @Override
+    public String toString() {
+        return "CategoryEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", color='" + color + '\'' +
+                ", user=" + user +
+                '}';
+    }
+}
