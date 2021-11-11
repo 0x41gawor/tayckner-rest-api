8c1bb6a2bca9a95982ef714fda5db4b33c3e69fb
0x41gawor <41gawor@gmail.com>
2021-11-12 00:26:41 +0100

    feat(repository): create entity class for User #12

---
+++ src/main/java/pl/gawor/tayckner/taycknerbackend/repository/entity/UserEntity.java
@@ -0,0 +1,109 @@
+package pl.gawor.tayckner.taycknerbackend.repository.entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Entity class for `User`.
+ *
+ * A simple POJO class with Hibernate annotations.
+ */
+@Entity
+@Table(name = "user")
+public class UserEntity {
+// -------------------------------------------------------------------------------------- F I E L D S
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "id")
+    private long id;
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+// -------------------------------------------------------------------------------------- C O N S T R U C T O R S
+    public UserEntity() {
+        // required by Hibernate
+    }
+
+    public UserEntity(long id, String username, String password, String firstName, String lastName, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+// -------------------------------------------------------------------------------------- G E T T E R S / S E T T E R S
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+// -------------------------------------------------------------------------------------- T O  S T R I N G
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
