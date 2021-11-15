52f5a8f26b85611bd27e26660ff48131250c133f
0x41gawor <41gawor@gmail.com>
2021-11-15 16:32:48 +0100

    feat(service): create CategoryMapper #15

---
+++ src/main/java/pl/gawor/tayckner/taycknerbackend/service/mapper/CategoryMapper.java
@@ -0,0 +1,25 @@
+package pl.gawor.tayckner.taycknerbackend.service.mapper;
+
+import pl.gawor.tayckner.taycknerbackend.core.model.CategoryModel;
+import pl.gawor.tayckner.taycknerbackend.repository.entity.CategoryEntity;
+
+/**
+ * Mapper used to map between Category Model and Entity classes.
+ *
+ */
+public class CategoryMapper implements Mapper<CategoryModel, CategoryEntity> {
+
+    @Override
+    public CategoryEntity mapToEntity(CategoryModel model) {
+        if(model == null) return null;
+        UserMapper userMapper = new UserMapper();
+        return new CategoryEntity(model.getId(), model.getName(), model.getDescription(), model.getColor(), userMapper.mapToEntity(model.getUser()));
+    }
+
+    @Override
+    public CategoryModel mapToModel(CategoryEntity entity) {
+        if(entity == null) return null;
+        UserMapper userMapper = new UserMapper();
+        return new CategoryModel(entity.getId(), entity.getName(), entity.getDescription(), entity.getColor(), userMapper.mapToModel(entity.getUser()));
+    }
+}
