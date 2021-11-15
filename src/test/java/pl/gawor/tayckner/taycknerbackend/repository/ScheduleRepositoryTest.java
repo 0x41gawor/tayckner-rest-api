13982f921ee8b5f2e91bd6a38db3a8ac19c2254e
0x41gawor <41gawor@gmail.com>
2021-11-15 16:03:21 +0100

    style(repository): few less line breaks in tests #16

--- src/test/java/pl/gawor/tayckner/taycknerbackend/repository/ScheduleRepositoryTest.java
+++ src/test/java/pl/gawor/tayckner/taycknerbackend/repository/ScheduleRepositoryTest.java
@@ -1,7 +1,5 @@
 package pl.gawor.tayckner.taycknerbackend.repository;
 
-
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
