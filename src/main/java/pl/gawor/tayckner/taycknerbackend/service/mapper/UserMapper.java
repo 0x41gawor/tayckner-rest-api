1a66b120ed712b567dbccf7075e465f80630264d
0x41gawor <41gawor@gmail.com>
2021-11-15 16:20:40 +0100

    feat(service): create UserMapper #15

---
+++ src/main/java/pl/gawor/tayckner/taycknerbackend/service/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package pl.gawor.tayckner.taycknerbackend.service.mapper;
+
+import pl.gawor.tayckner.taycknerbackend.core.model.UserModel;
+import pl.gawor.tayckner.taycknerbackend.repository.entity.UserEntity;
+
+/**
+ * Mapper used to map between User Model and Entity classes.
+ *
+ */
+public class UserMapper implements Mapper<UserModel, UserEntity> {
+
+    @Override
+    public UserEntity mapToEntity(UserModel model) {
+        if (model == null) return null;
+        return new UserEntity(model.getId(), model.getUsername(), model.getPassword(), model.getFirstName(), model.getLastName(), model.getEmail());
+    }
+
+    @Override
+    public UserModel mapToModel(UserEntity entity) {
+        if (entity == null) return null;
+        return new UserModel(entity.getId(), entity.getUsername(), entity.getPassword(), entity.getFirstName(), entity.getLastName(), entity.getEmail());
+    }
+}
