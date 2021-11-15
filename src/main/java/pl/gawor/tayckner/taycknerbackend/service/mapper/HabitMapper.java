d7fc1568f1b266badade232ac2f0762618032ff2
0x41gawor <41gawor@gmail.com>
2021-11-15 16:45:30 +0100

    feat(service): create HabitMapper #15

---
+++ src/main/java/pl/gawor/tayckner/taycknerbackend/service/mapper/HabitMapper.java
@@ -0,0 +1,24 @@
+package pl.gawor.tayckner.taycknerbackend.service.mapper;
+
+import pl.gawor.tayckner.taycknerbackend.core.model.HabitModel;
+import pl.gawor.tayckner.taycknerbackend.repository.entity.HabitEntity;
+
+/**
+ * Mapper used to map between Habit Model and Entity classes.
+ *
+ */
+public class HabitMapper implements Mapper<HabitModel, HabitEntity> {
+    @Override
+    public HabitEntity mapToEntity(HabitModel model) {
+        if (model == null) return null;
+        UserMapper userMapper = new UserMapper();
+        return new HabitEntity(model.getId(), model.getName(), model.getColor(), userMapper.mapToEntity(model.getUser()));
+    }
+
+    @Override
+    public HabitModel mapToModel(HabitEntity entity) {
+        if (entity == null) return null;
+        UserMapper userMapper = new UserMapper();
+        return new HabitModel(entity.getId(), entity.getName(), entity.getColor(), userMapper.mapToModel(entity.getUser()));
+    }
+}
