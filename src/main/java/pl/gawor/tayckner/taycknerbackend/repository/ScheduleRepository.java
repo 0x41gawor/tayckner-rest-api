b46f90675f732f676ee5ee969f9b78b0068c13e9
0x41gawor <41gawor@gmail.com>
2021-11-15 15:49:20 +0100

    feat(repository): create ScheduleRepository #16

---
+++ src/main/java/pl/gawor/tayckner/taycknerbackend/repository/ScheduleRepository.java
@@ -0,0 +1,17 @@
+package pl.gawor.tayckner.taycknerbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.gawor.tayckner.taycknerbackend.repository.entity.ScheduleEntity;
+import pl.gawor.tayckner.taycknerbackend.repository.entity.UserEntity;
+
+import java.util.List;
+
+/**
+ * Repository class for `Schedule`.
+ *
+ */
+@Repository
+public interface ScheduleRepository extends JpaRepository<ScheduleEntity, Long> {
+    List<ScheduleEntity> findScheduleEntitiesByUser(UserEntity user);
+}
