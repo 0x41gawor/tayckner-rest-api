4e563a85eff248ed3538599204a9c45efb973362
0x41gawor <41gawor@gmail.com>
2021-11-16 21:13:04 +0100

    docs(service): add javadoc to CRUDService interface #17

--- src/main/java/pl/gawor/tayckner/taycknerbackend/service/service/CRUDService.java
+++ src/main/java/pl/gawor/tayckner/taycknerbackend/service/service/CRUDService.java
@@ -9,9 +9,53 @@ import java.util.List;
  *</p>
  */
 public interface CRUDService<Model> {
+    /**
+     * List CRUD method.
+     *
+     * Note that this method can return an empty list.
+     *
+     * @return List of all models in database
+     */
     List<Model> list();
+    /**
+     * Create CRUD method.
+     * <P>
+     *     This method sets model id to 0, because when Hibernate sees objects with such id, he knows he has to add new row in relation.
+     * </P>
+     * @param model model that has to be created
+     * @return created model
+     */
     Model create(Model model);
+    /**
+     * Read CRUD method.
+     *
+     * <P>
+     *      If no object exists with given id method returns null
+     * </P>
+     * @param id id of model to be returned
+     * @return model with given id (or null)
+     */
     Model read(long id);
+    /**
+     * Update CRUD method.
+     *
+     * <P>
+     *     Method replaces object with given id with new model
+     * </P>
+     * @param id id of model to be updated
+     * @param model new model
+     * @return new updated model saved to the database
+     */
     Model update(long id, Model model);
+    /**
+     * Delete CRUD method.
+     *
+     * <P>
+     *     Method checks if object with given id exits if not it returns false, <br>
+     *     if object exists and deletion was successful it returns true.
+     * </P>
+     * @param id id of model to be deleted
+     * @return true of false
+     */
     boolean delete(long id);
 }
