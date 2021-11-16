d18a328797ef2d99036d290ca86521d877a6eaf5
0x41gawor <41gawor@gmail.com>
2021-11-16 21:21:32 +0100

    feat(service): create User Service #17

---
+++ src/main/java/pl/gawor/tayckner/taycknerbackend/service/service/UserService.java
@@ -0,0 +1,67 @@
+package pl.gawor.tayckner.taycknerbackend.service.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.gawor.tayckner.taycknerbackend.core.model.UserModel;
+import pl.gawor.tayckner.taycknerbackend.repository.UserRepository;
+import pl.gawor.tayckner.taycknerbackend.repository.entity.UserEntity;
+import pl.gawor.tayckner.taycknerbackend.service.service.mapper.UserMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Service class for User.
+ */
+@Service
+public class UserService implements CRUDService<UserModel> {
+
+    private final UserRepository repository;
+    private final UserMapper mapper;
+
+    @Autowired
+    public UserService(UserRepository repository, UserMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public List<UserModel> list() {
+        List<UserEntity> entities = repository.findAll();
+        List<UserModel> models = new ArrayList<>();
+        for (UserEntity entity : entities) {
+            models.add(mapper.mapToModel(entity));
+        }
+        return models;
+    }
+
+    @Override
+    public UserModel create(UserModel model) {
+        model.setId(0);
+        UserEntity entity = mapper.mapToEntity(model);
+        return mapper.mapToModel(repository.save(entity));
+    }
+
+    @Override
+    public UserModel read(long id) {
+        Optional<UserEntity> optional = repository.findById(id);
+        UserEntity entity = optional.orElse(null);
+        return mapper.mapToModel(entity);
+    }
+
+    @Override
+    public UserModel update(long id, UserModel model) {
+        UserEntity entity = mapper.mapToEntity(model);
+        return mapper.mapToModel(repository.save(entity));
+    }
+
+    @Override
+    public boolean delete(long id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
