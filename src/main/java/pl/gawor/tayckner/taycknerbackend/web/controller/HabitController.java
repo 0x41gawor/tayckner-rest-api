2115ce9f01992ca217f0116ccd84df3e0c781cd4
0x41gawor <41gawor@gmail.com>
2021-11-17 23:53:39 +0100

    feat(web): create habit controller #25

---
+++ src/main/java/pl/gawor/tayckner/taycknerbackend/web/controller/HabitController.java
@@ -0,0 +1,57 @@
+package pl.gawor.tayckner.taycknerbackend.web.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import pl.gawor.tayckner.taycknerbackend.core.model.HabitModel;
+
+/**
+ * Controller class for CRUDing Habits.
+ */
+@RestController
+@RequestMapping("/api/habits")
+public class HabitController {
+
+    // -------------------------------------------------------------------------------------- L I S T
+    @GetMapping(
+            value = "",
+            produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public String list() {
+        return "list";
+    }
+    // -------------------------------------------------------------------------------------- C R E A T E
+    @PostMapping(
+            value = "",
+            consumes = {MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public String create(@RequestBody HabitModel model) {
+        return "create";
+    }
+    // -------------------------------------------------------------------------------------- R E A D
+    @GetMapping(
+            value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public String read(@PathVariable(name = "id") long id) {
+        return "read";
+    }
+    // -------------------------------------------------------------------------------------- U P D A T E
+    @PutMapping(
+            value = "/{id}",
+            consumes = {MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public String  update(@PathVariable(name = "id") long id, @RequestBody HabitModel model) {
+        return "update";
+    }
+    // -------------------------------------------------------------------------------------- D E L E T E
+    @DeleteMapping(
+            value = "{id}",
+            produces = "text/plain"
+    )
+    public String delete(@PathVariable(name = "id") long id) {
+        return "delete";
+    }
+
+}
