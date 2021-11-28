eb69b6a104c7c34f3c78c75b07437f9557b91f95
0x41gawor <41gawor@gmail.com>
2021-11-28 02:25:53 +0100

    feat(service): add logger for ScheduleService #30

--- src/main/java/pl/gawor/tayckner/taycknerbackend/service/service/ScheduleService.java
+++ src/main/java/pl/gawor/tayckner/taycknerbackend/service/service/ScheduleService.java
@@ -1,5 +1,7 @@
 package pl.gawor.tayckner.taycknerbackend.service.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.gawor.tayckner.taycknerbackend.core.model.ScheduleModel;
@@ -24,6 +26,8 @@ public class ScheduleService implements CRUDService<ScheduleModel> {
 
     private final UserMapper userMapper;
 
+    private final Logger logger = LoggerFactory.getLogger(ScheduleService.class);
+
     @Autowired
     public ScheduleService(ScheduleRepository repository, ScheduleMapper mapper, UserMapper userMapper) {
         this.repository = repository;
@@ -34,45 +38,59 @@ public class ScheduleService implements CRUDService<ScheduleModel> {
     // ------------------------------------------------------------------------------------------ L I S T
     @Override
     public List<ScheduleModel> list() {
+        logger.info("ScheduleService :: list()");
         List<ScheduleEntity> entities = repository.findAll();
         List<ScheduleModel> models = new ArrayList<>();
         for (ScheduleEntity entity : entities) {
             models.add(mapper.mapToModel(entity));
         }
+        logger.info("ScheduleService :: list() = {}", models);
         return models;
     }
 
     // -------------------------------------------------------------------------------------- C R E A T E
     @Override
     public ScheduleModel create(ScheduleModel model) {
+        logger.info("ScheduleService :: create(model = {})", model);
         model.setId(0);
         ScheduleEntity entity = mapper.mapToEntity(model);
-        return mapper.mapToModel(repository.save(entity));
+        ScheduleModel createdModel =  mapper.mapToModel(repository.save(entity));
+        logger.info("ScheduleService :: create(model = {}) = {}", model, createdModel);
+        return createdModel;
     }
 
     // ------------------------------------------------------------------------------------------ R E A D
     @Override
     public ScheduleModel read(long id) {
+        logger.info("ScheduleService :: read(id = {})", id);
         Optional<ScheduleEntity> optional = repository.findById(id);
         ScheduleEntity entity = optional.orElse(null);
-        return mapper.mapToModel(entity);
+        ScheduleModel readModel =  mapper.mapToModel(entity);
+        logger.info("ScheduleService :: read(id = {}) = {}", id, readModel);
+        return readModel;
     }
 
     // -------------------------------------------------------------------------------------- U P D A T E
     @Override
     public ScheduleModel update(long id, ScheduleModel model) {
+        logger.info("ScheduleService :: update(id = {}, model = {})", id, model);
         ScheduleEntity entity = mapper.mapToEntity(model);
         entity.setId(id);
-        return mapper.mapToModel(repository.save(entity));
+        ScheduleModel updatedModel =  mapper.mapToModel(repository.save(entity));
+        logger.info("ScheduleService :: update(id = {}, model = {}) = {}", id, model, updatedModel);
+        return updatedModel;
     }
 
     // -------------------------------------------------------------------------------------- D E L E T E
     @Override
     public boolean delete(long id) {
+        logger.info("ScheduleService :: delete(id = {})", id);
         if (repository.existsById(id)) {
             repository.deleteById(id);
+            logger.info("ScheduleService :: delete(id = {}) = true", id);
             return true;
         }
+        logger.info("ScheduleService :: delete(id = {}) = false", id);
         return false;
     }
     // -------------------------------------------------------------------------- L I S T   B Y   U S E R
@@ -87,12 +105,14 @@ public class ScheduleService implements CRUDService<ScheduleModel> {
      * @return List of all models in database, that has given user
      */
     public List<ScheduleModel> list(UserModel user) {
+        logger.info("ScheduleService :: list(user = {})", user);
         UserMapper userMapper = new UserMapper();
         List<ScheduleEntity> entities = repository.findScheduleEntitiesByUser(userMapper.mapToEntity(user));
         List<ScheduleModel> models = new ArrayList<>();
         for (ScheduleEntity entity : entities) {
             models.add(mapper.mapToModel(entity));
         }
+        logger.info("ScheduleService :: list(user = {}) = {}", user, models);
         return models;
     }
     // ---------------------------------------------------- E X I S T S   B Y   U S E R   A N D   N A M E
@@ -105,7 +125,10 @@ public class ScheduleService implements CRUDService<ScheduleModel> {
      * @return true if Schedule with given user and name exists
      */
     public boolean existByName(String name, UserModel user) {
-        return repository.existsByNameAndUser(name, userMapper.mapToEntity(user));
+        logger.info("ScheduleService :: existByName(name = {}, user = {})", name, user);
+        boolean result = repository.existsByNameAndUser(name, userMapper.mapToEntity(user));
+        logger.info("ScheduleService :: existByName(name = {}, user = {}) = {}", name, user, result);
+        return result;
     }
     // --------------------------------------------------------- E X I S T S   B Y  I D   A N D   U S E R
 
@@ -117,6 +140,9 @@ public class ScheduleService implements CRUDService<ScheduleModel> {
      * @return true if Schedule with given user and id exists
      */
     public boolean existsByIdAndUser(long id, UserModel user) {
-        return repository.existsByIdAndUser(id, userMapper.mapToEntity(user));
+        logger.info("ScheduleService :: existsById(id = {}, user = {})", id, user);
+        boolean result = repository.existsByIdAndUser(id, userMapper.mapToEntity(user));
+        logger.info("ScheduleService :: existsById(id = {}, user = {}) = {}", id, user, result);
+        return  result;
     }
 }
